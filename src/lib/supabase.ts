8ccf108d5b96acd8bc40d7fab374a8ab3be32ad1
Kamdrin1 <youremail@gmail.com>
2026-06-24 20:26:16 -0700

    Use hardcoded Supabase credentials for testing

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,6 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     'https://lszifwrtshljohauwnfq.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzemkxZndydHNobGpvaGF1d25maSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzE5MzMyNDI4LCJleHAiOjE3MzQ5ODQyMjh9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxzemlmd3J0c2hsam9oYXV3bmZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDM5OTQsImV4cCI6MjA5NDYxOTk5NH0.j5BkXL8Xfd0MlGIVAtxuLkL5fp-QBu72vxkT-MHMkz4.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
   )
 }
